d59b5f8a076742e217fc4b4feab2870dcd07bb31
ItaloBrazucaDeveloper <italo.ter.pes@gmail.com>
2025-06-11 13:26:17 -0300

    fix(DED): fixed render of errors pages

--- src/Core/DED/RenderError.php
+++ src/Core/DED/RenderError.php
@@ -17,7 +17,7 @@ class RenderError {
 
     $view = $twig->has("@error-page/{$viewName}.twig") ? $viewName : 'default';
     while (ob_get_level()) ob_end_clean();
-    echo $twig->render("@error-page/{$view}", $viewData);
+    echo $twig->render("@error-page/{$view}.twig", $viewData);
   }
 
   private static function getViewNameFromClassName(
