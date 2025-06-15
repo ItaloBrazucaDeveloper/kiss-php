fe1e000c486f00140981e7f79a5493487a72ee3f
ItaloBrazucaDeveloper <italo.ter.pes@gmail.com>
2025-06-15 18:36:27 -0300

    implement(ViewRender): implement ViewParams to user can modify/add functions or globals on twig

--- src/Services/ViewRender.php
+++ src/Services/ViewRender.php
@@ -1,7 +1,7 @@
 <?php
 namespace KissPhp\Services;
 
-use KissPhp\Support\Env;
+use KissPhp\Support\ViewParams;
 use KissPhp\Exceptions\NotFound;
 use KissPhp\Exceptions\ViewRenderException;
 use KissPhp\Config\{ PathsConfig, ViewRenderConfig };
@@ -19,25 +19,21 @@ class ViewRender {
       $loader = new \Twig\Loader\FilesystemLoader(rootPath: PathsConfig::VIEWS_PATH);
       $loader->addPath('');
       $loader->addPath(PathsConfig::INFRA_VIEWS_PATH, 'infra');
+
       foreach (ViewRenderConfig::ALIAS_PATHS as $path => $alias) {
         $loader->addPath($path, $alias);
       }
       $this->twig = new \Twig\Environment($loader, ViewRenderConfig::ENVORIMENT);
+
+      foreach (ViewParams::getFunctions() as $functionName => $closure) {
+        $this->twig->addFunction(new \Twig\TwigFunction($functionName, $closure));
+      }
+      foreach (ViewParams::getGlobals() as $globalName => $global) {
+        $this->twig->addGlobal($globalName, $global);
+      }
     } catch (\Exception $e) {
       throw new ViewRenderException("Failed to initialize Twig environment", 0, $e);
     }
-
-    $this->twig->addFunction(new \Twig\TwigFunction(
-      'getInputError',
-      function($inputName) {
-        $errors = $_SESSION['InputErrors'][$inputName] ?? '';
-        unset($_SESSION['InputErrors'][$inputName]);
-        return $errors;
-      }
-    ));
-
-    $isDevMode = Env::get('DEV_MODE') ?? 'false';
-    $this->twig->addGlobal('DEV_MODE', strtolower($isDevMode) === 'true');
   }
 
   public function render(string $viewName, array $params = []): string {
---
+++ src/Support/ViewParams.php
@@ -0,0 +1,49 @@
+<?php
+namespace KissPhp\Support;
+
+final class ViewParams {
+  private static array $functions = [
+    'getInputError' => function ($inputName) {
+      $errors = $_SESSION['InputErrors'][$inputName] ?? '';
+      unset($_SESSION['InputErrors'][$inputName]);
+      return $errors;
+    }
+  ];
+
+  private static array $globals = [
+    'session' => $_SESSION,
+    'DEV_MODE' => Env::get('DEV_MODE') === 'true',
+  ];
+
+  /**
+   * @return \Closure[]
+   */
+  public static function getFunctions(): array {
+    return self::$functions;
+  }
+
+  /**
+   * Adiciona funções que podem ser usadas de qualquer view com $twig.
+   * 
+   * @param \Closure[] $functions
+   */
+  public static function addFunctions(array $functions) {
+    self::$functions = array_merge(self::$functions, $functions);
+  }
+
+  /**
+   * @return array<string, mixed>
+   */
+  public static function getGlobals(): array {
+    return self::$globals;
+  }
+
+  /**
+   * Adiciona valores globais que podem ser acesados de qualquer view com $twig.
+   * 
+   * @param array<string, mixed> $globals
+   */
+  public static function addGlobals(array $globals) {
+    self::$globals = array_merge(self::$globals, $globals);
+  }
+}
